e7158ce8a90676bdf0f5f9772ab2ad3a6275ef6e
naoto yoshida <yoshida-naoto@social-sharing.org>
2017-08-03 17:30:54 +0900

    タイトルの変更

--- fuel/app/views/common/header.php
+++ fuel/app/views/common/header.php
@@ -2,7 +2,7 @@
 <html>
 <head>
 	<meta http-equiv="Content-Type" content="text/html;charset=utf-8" />
-	<title>マジカリドル</title>
+	<title>私の世界の見方</title>
 	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
 	<?php echo Asset::js("ajax.js"); ?>
 </head>
