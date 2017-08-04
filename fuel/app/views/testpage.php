8e87aac2f86df709e19a7ebaecc20b28a7b0d08d
naoto yoshida <yoshida-naoto@social-sharing.org>
2017-08-04 15:14:13 +0900

    テストページ、ヘッダーの編集

--- fuel/app/views/testpage.php
+++ fuel/app/views/testpage.php
@@ -46,14 +46,14 @@ document.layers[obj+"i"].src=op:document.layers[obj+"i"].src=cl
 
 <div>
 	<?= Form::open(array('action' => 'testpage/selectPlayer', 'method' => 'post'));?>
-		<select name=”player”>
-		<option value=”3”>3人</option>
-		<option value=”4”>4人</option>
-		<option value=”5”>5人</option>
-		<option value=”6”>6人</option>
-		<option value=”7”>7人</option>
-		<option value=”8”>8人</option>
-		<option value=”9”>9人</option>
+		<select name= player>
+		<option value= 3>3人</option>
+		<option value= 4>4人</option>
+		<option value= 5>5人</option>
+		<option value= 6>6人</option>
+		<option value= 7>7人</option>
+		<option value= 8>8人</option>
+		<option value= 9>9人</option>
 		</select>
 	<?= Form::button(null, '決定', array('type' => 'submit', 'style' => 'padding: 2px;')); ?>
 	<?= Form::close(); ?>
