e2907d09a31d9dc65bd58ca42f052dfad95c3c1b
naoto yoshida <yoshida-naoto@social-sharing.org>
2017-08-04 17:21:31 +0900

    テストページの編集

--- fuel/app/classes/controller/inputName.php
+++ fuel/app/classes/controller/inputName.php
@@ -4,6 +4,8 @@ class Controller_inputName extends Controller_Base_Game
 {
 	public function action_index()
 	{
+		var_dump("aaa");
+		exit;
 		View_Wrap::contents('inputName', $this->view_data);
 	}
 }
