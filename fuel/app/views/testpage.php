6ad8edac2395a9899930089576f6f8dab0e118ec
naoto yoshida <yoshida-naoto@social-sharing.org>
2017-08-04 12:06:37 +0900

    テストページの編集

--- fuel/app/views/testpage.php
+++ fuel/app/views/testpage.php
@@ -1,1 +1,43 @@
-<p>Hello World!</p>
+<marquee>Hello World!</marquee>
+
+<script language="javascript">
+<!--
+
+new Image().src = "img/HP_Triangle_TR12pxc008080.gif";
+op="img/HP_Triangle_TR12pxc008080.gif";
+cl="img/HP_Triangle_TD12pxc008080.gif";
+function dd(obj){
+
+if(document.getElementById)
+{document.getElementById(obj)
+.style.display=='none'?document.getElementById(obj)
+.style.display='':document.getElementById(obj)
+.style.display='none';
+document.getElementById(obj+"i").src.indexOf('TD12pxc008080')>0?
+document.getElementById(obj+"i").src=op:
+document.getElementById(obj+"i").src=cl 
+
+} else if(document.all){
+document.all(obj).style.display=='none'?
+document.all(obj).style.display='':document.all(obj).style.display='none';
+document.all(obj+"i").src.indexOf('TD12pxc008080')>0?
+document.all(obj+"i").src=op:document.all(obj+"i").src=cl
+
+} else if(document.layers){
+document.layers[obj].display=='none'?
+document.layers[obj].display='':document.layers[obj].display='none';
+document.layers[obj+"i"].src.indexOf('TD12pxc008080')>0?
+document.layers[obj+"i"].src=op:document.layers[obj+"i"].src=cl
+}
+}
+
+//-->
+</script>
+
+<div id="折りたたみB">
+<a href="javascript:dd('折りたたみ指示B')"><img src="img/HP_Triangle_TR12pxc008080.gif" id="折りたたみ指示Bi" style="border-width : 0px;" alt="右三角印">　ランキング</a>
+<div style="display:none;margin-left:18pt" id="折りたたみ指示B">1位<br>
+２位<br>
+３位<br>
+</div>
+</div>
