c2d025912e944f65b9e711ca3d81a4e60f93af39
naoto yoshida <yoshida-naoto@social-sharing.org>
2017-08-08 17:25:33 +0900

    static削除

--- fuel/app/classes/controller/setplayernumber.php
+++ fuel/app/classes/controller/setplayernumber.php
@@ -12,7 +12,7 @@ class Controller_Setplayernumber extends Controller_Base_Game
 		$param = input::post();
 		//人数分の手札配列を作る処理
 		
-		$this->Model_tableinfo::setPlayerArrayValue($$param);
+		$this->Model_tableinfo('setPlayerArrayValue()',$param);
 		//プレイヤー一覧の配列初期化用のメソッド
 		var_dump(Model_tableinfo::$playerArry);
 		exit;
