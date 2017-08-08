ce4b9d687443ec04784a0368dd987ec50a35f762
naoto yoshida <yoshida-naoto@social-sharing.org>
2017-08-08 11:17:02 +0900

    修正

--- fuel/app/classes/controller/setplayernumber.php
+++ fuel/app/classes/controller/setplayernumber.php
@@ -12,6 +12,8 @@ class Controller_Setplayernumber extends Controller_Base_Game
 		$param = input::post();
 		//人数分の手札配列を作る処理
 		
+		$this->view_data['playerNumber'] = $param;
+		
 		//名前入力画面に遷移
 		View_Wrap::contents('entername',$this->view_data);
 	}
--- fuel/app/views/title.php
+++ fuel/app/views/title.php
@@ -1,2 +1,2 @@
 <H1>私の世界の見方(仮題)</H1>
-<button onclick="location.href='setplayernumber'" value="スタート"></button>
+<button onclick="location.href='setplayernumber'">スタート</button>
