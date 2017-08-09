8630031d55cd527d5558325c8c11158227efce99
naoto yoshida <yoshida-naoto@social-sharing.org>
2017-08-09 16:59:29 +0900

    shuffle処理

--- fuel/app/classes/controller/entername.php
+++ fuel/app/classes/controller/entername.php
@@ -20,6 +20,7 @@ class Controller_entername extends Controller_Base_Game
 		//山札から手札を配る
 		Model_Handinfo::createPlayerHand();
 		
+		var_dump("aaaa");
 		printf(Model_tableinfo::$playerNumber);
 		exit;
 		//一番目のプレイヤーの待機画面に遷移
