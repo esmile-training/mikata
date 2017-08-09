435aaf4c7f16d56e6b22572f00df953b950ecf8a
naoto yoshida <yoshida-naoto@social-sharing.org>
2017-08-09 12:18:31 +0900

    修正

--- fuel/app/classes/controller/entername.php
+++ fuel/app/classes/controller/entername.php
@@ -10,7 +10,9 @@ class Controller_entername extends Controller_Base_Game
 	public function action_setPlayerName()
 	{
 		$param = input::post();
-		
+		var_dump($param);
 		Model_tableinfo::setPlayerArrayValue($param);
+		var_dump(Model_tableinfo::$playerArray);
+		exit;
 	}
 }
