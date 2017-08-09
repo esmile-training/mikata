bd20c46d6373df22bfb1096f44e710570863988c
naoto yoshida <yoshida-naoto@social-sharing.org>
2017-08-09 14:34:49 +0900

    修正

--- fuel/app/classes/controller/setplayernumber.php
+++ fuel/app/classes/controller/setplayernumber.php
@@ -10,12 +10,11 @@ class Controller_Setplayernumber extends Controller_Base_Game
 	public function action_getPlayerNumber()
 	{
 		$param = input::post();
-		//人数分の手札配列を作る処理
-		
+
+		//人数を変数に格納
 		Model_tableinfo::$playerNumber = $param['player'];
-		Model_tableinfo::setPlayerArrayValue($param['player']);
 		
 		//名前入力画面に遷移
-		View_Wrap::contents('entername',$this->view_data);
+		Response::redirect('enterName');
 	}
 }
