8bdaa2fab157d894b0d571bd24db0ad0e8cfea94
naoto yoshida <yoshida-naoto@social-sharing.org>
2017-08-09 11:56:35 +0900

    テスト

--- fuel/app/views/entername.php
+++ fuel/app/views/entername.php
@@ -1,3 +1,10 @@
 <h1>プレイヤー名を順番に入力してください</h1>
-<?php print_r(Model_tableinfo::$playerNumber);
-//人数分のテキストボックスを生成する処理(for文) ?>
+<div>
+<?= Form::open(array('action' => 'entername/setPlayerName', 'method' => 'post'));
+	print_r(Model_tableinfo::$playerNumber);
+	for ($n = 1; $n <= Model_tableinfo::$playerNumber; $n++)
+	{
+		print_r($n.'<input type="text"><br>');
+	}
+?>
+</div>
