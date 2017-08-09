5e672b10b6787c7606d269760a8093cb299b25fd
naoto yoshida <yoshida-naoto@social-sharing.org>
2017-08-09 15:44:16 +0900

    dump

--- fuel/app/classes/controller/entername.php
+++ fuel/app/classes/controller/entername.php
@@ -16,6 +16,7 @@ class Controller_entername extends Controller_Base_Game
 		
 		//山札から手札を配る
 		Model_Handinfo::createPlayerHand();
+		var_dump(Model_Handinfo::$handarray[1]);
 		
 		//一番目のプレイヤーの待機画面に遷移
 		Response::redirect('testpage');
