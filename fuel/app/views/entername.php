cc57de8561ebf3f62db3e1b3fc7f9311f932e304
naoto yoshida <yoshida-naoto@social-sharing.org>
2017-08-09 11:57:59 +0900

    テスト

--- fuel/app/views/entername.php
+++ fuel/app/views/entername.php
@@ -1,10 +1,9 @@
 <h1>プレイヤー名を順番に入力してください</h1>
 <div>
 <?= Form::open(array('action' => 'entername/setPlayerName', 'method' => 'post'));
-	print_r(Model_tableinfo::$playerNumber);
 	for ($n = 1; $n <= Model_tableinfo::$playerNumber; $n++)
 	{
-		print_r($n.'<input type="text"><br>');
+		print_r('プレイヤー'.$n.'.<input type="text"><br>');
 	}
 ?>
 </div>
