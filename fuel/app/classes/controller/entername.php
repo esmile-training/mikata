2ba0e5cb5b5ff162c42c4dbd7bf3344ef5d0a41b
naoto yoshida <yoshida-naoto@social-sharing.org>
2017-08-09 17:33:22 +0900

    shuffle処理

--- fuel/app/classes/controller/entername.php
+++ fuel/app/classes/controller/entername.php
@@ -23,6 +23,6 @@ class Controller_entername extends Controller_Base_Game
 		var_dump(Model_tableinfo::$playerNumber);
 		//一番目のプレイヤーの待機画面に遷移
 		//Response::redirect('testpage');
-		View_Wrap::contents('testpage');
+		View_Wrap::contents('testpage', $this->view_data);
 	}
 }
