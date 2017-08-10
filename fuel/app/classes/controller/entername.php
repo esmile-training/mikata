3ee77571ff61a5b1d51f5f93d8ed8e1474cb4b05
naoto yoshida <yoshida-naoto@social-sharing.org>
2017-08-10 16:38:55 +0900

    pictest

--- fuel/app/classes/controller/entername.php
+++ fuel/app/classes/controller/entername.php
@@ -23,7 +23,6 @@ class Controller_entername extends Controller_Base_Game
 		$this->game['hand'] = Model_Handinfo::createPlayerHand();
 		
 		print_r($this->game);
-		exit;
 		//一番目のプレイヤーの待機画面に遷移
 		//Response::redirect('testpage');	//実験的にコメントアウト
 		View_Wrap::contents('testpage', $this->view_data, $this->game);	//実験的に記述
