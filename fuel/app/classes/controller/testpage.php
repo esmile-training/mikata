32c655c7fcf830acd9c3d553023fbfd00fc54ce1
naoto yoshida <yoshida-naoto@social-sharing.org>
2017-08-10 14:17:04 +0900

    pictest

--- fuel/app/classes/controller/testpage.php
+++ fuel/app/classes/controller/testpage.php
@@ -17,7 +17,7 @@ class Controller_Testpage extends Controller_Base_Game
 	
 	public function action_picthatcard()
 	{
-		print_r(Model_Handinfo::$handarray);
+		print_r("handonfo".Model_Handinfo::$handarray);
 		exit;
 		Model_Handinfo::picThatCard();
 		Response::redirect('testpage');
