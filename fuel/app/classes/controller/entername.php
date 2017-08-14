b6fb908a2d2f93ddcbb014e4e4476c9f2e3c5ef5
naoto yoshida <frandle_2@Lvateinn>
2017-08-14 12:05:38 +0900

    test

--- fuel/app/classes/controller/entername.php
+++ fuel/app/classes/controller/entername.php
@@ -22,6 +22,9 @@ class Controller_entername extends Controller_Base_Game
 		
 		$this->view_data['game'] = $this->game;
                 
+                print_r($this->user);
+                exit;
+                
 		//一番目のプレイヤーの待機画面に遷移
 		Response::redirect('testpage');	//実験的にコメントアウト
 		//View_Wrap::contents('testpage', $this->view_data, $this->game);	//実験的に記述
