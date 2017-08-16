3ba2e1e4df3f6258109bda50d968a2a0b3629774
naoto yoshida <yoshida-naoto@social-sharing.org>
2017-08-16 12:40:40 +0900

    test

--- fuel/app/views/testpage.php
+++ fuel/app/views/testpage.php
@@ -52,11 +52,11 @@ document.layers[obj+"i"].src=op:document.layers[obj+"i"].src=cl
 
 <?= Form::open(array('action' => 'testpage/picthatcard', 'method' => 'post'));?>
 <?= Form::hidden('player', implode(",",$game['player'])); ?>
-<?= Form::hidden('themeStock', implode("\t",$game['card']['themeStock'])); ?>
-<?= Form::hidden('anserStock', implode("\t",$game['card']['anserStock'])); ?>
+<?= Form::hidden('themeStock', implode(",",$game['card']['themeStock'])); ?>
+<?= Form::hidden('anserStock', implode(",",$game['card']['anserStock'])); ?>
 <?php foreach ($game['player'] as $value) 
 	{
-		echo Form::hidden($value, implode("\t",$game['hand'][$value]));
+		echo Form::hidden($value, implode(",",$game['hand'][$value]));
 	} 
 ?>
 <?= Form::button(null, '一枚引く', array('type' => 'submit', 'style' => 'padding: 2px;')); ?>
