673f8c485be5b035f90d6b8b52ac06963b0b8256
naoto yoshida <yoshida-naoto@social-sharing.org>
2017-08-16 16:20:12 +0900

    test

--- fuel/app/classes/controller/testpage.php
+++ fuel/app/classes/controller/testpage.php
@@ -24,7 +24,7 @@ class Controller_Testpage extends Controller_Base_Game
 		}
 		
 		$game['table'] = array('status' => $param['status'], 'thatCardArray' => $param['thatCardArray'], 'votesArray' => $param['votesArray'],'player' => $param['player'], 'deck'=>array('themeStock' => $param['themeStock'], 'anserStock' => $param['anserStock']));
-		print_r($game);exit;
+		
 		foreach ($game['table']['player'] as $value)
 		{
 			$game['hand'][$value] = $param[$value];
