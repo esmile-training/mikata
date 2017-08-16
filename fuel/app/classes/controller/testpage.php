aa289d8654e9188a136d9b17939cce8b48e8677e
naoto yoshida <yoshida-naoto@social-sharing.org>
2017-08-16 16:17:26 +0900

    test

--- fuel/app/classes/controller/testpage.php
+++ fuel/app/classes/controller/testpage.php
@@ -17,13 +17,13 @@ class Controller_Testpage extends Controller_Base_Game
 	public function action_picthatcard()
 	{
 		$param = input::post();
-
+		print_r($param);
+		exit;
+		
 		foreach ($param as $key => $value) 
 		{
 			$param[$key] = explode(",",$value);
 		}
-		print_r($param);
-		exit;
 		
 		$game['table'] = array('status' => $param['status'], 'thatCardArray' => $param['thatCardArray'], 'votesArray' => $param['votesArray'],'player' => $param['player'], 'deck'=>array('themeStock' => $param['themeStock'], 'anserStock' => $param['anserStock']));
 		foreach ($game['table']['player'] as $value)
