d4f36e327f94d4b3374ec570b0530d0e6b7a769e
naoto yoshida <yoshida-naoto@social-sharing.org>
2017-08-16 14:30:05 +0900

    test

--- fuel/app/views/testpage.php
+++ fuel/app/views/testpage.php
@@ -51,7 +51,7 @@ document.layers[obj+"i"].src=op:document.layers[obj+"i"].src=cl
 <?= View::forge('popup/wrap', array('name' => 'def', 'size' => 'big')); ?>
 
 <?= Form::open(array('action' => 'testpage/picthatcard', 'method' => 'post'));?>
-<?= $game = Model_tableinfo::$playerNumber ?>
+<?= $game = Model_datapros::$test ?>
 
 <?= Form::button(null, '一枚引く', array('type' => 'submit', 'style' => 'padding: 2px;')); ?>
 <?= Form::close(); ?>
