da965505439fe8b3815360b37618ba1a89d6b48b
naoto yoshida <yoshida-naoto@social-sharing.org>
2017-08-16 16:22:04 +0900

    test

--- fuel/app/classes/controller/testpage.php
+++ fuel/app/classes/controller/testpage.php
@@ -23,7 +23,8 @@ class Controller_Testpage extends Controller_Base_Game
 			$param[$key] = explode(",",$value);
 		}
 		
-		$game['table'] = array('status' => $param['status'], 'thatCardArray' => $param['thatCardArray'], 'votesArray' => $param['votesArray'],'player' => $param['player'], 'deck'=>array('themeStock' => $param['themeStock'], 'anserStock' => $param['anserStock']));
+		$game['table'] = array('status' => $param['status'], 'thatCardArray' => $param['thatCardArray'], 'votesArray' => $param['votesArray'],'player' => $param['player']);
+		$game['deck'] = array('deck'=>array('themeStock' => $param['themeStock'], 'anserStock' => $param['anserStock']));
 		
 		foreach ($game['table']['player'] as $value)
 		{
