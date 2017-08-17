5c3ad971ad7f32640d3c5d562e20200a80471568
naoto yoshida <yoshida-naoto@social-sharing.org>
2017-08-17 18:11:01 +0900

    test

--- fuel/app/classes/controller/confPlayer.php
+++ fuel/app/classes/controller/confPlayer.php
@@ -4,6 +4,8 @@ class Controller_confPlayer  extends Controller_Base_Game
 {
 	public function action_index()
 	{
+		$this->view_data['mikata_answer'] = $this->csv->getAll('/mikata/answer');
+		
 		View_Wrap::contents('confPlayer',$this->view_data);
 	}
 }
