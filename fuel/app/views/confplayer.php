2f5b4ae50c16d22bfabd9158ef68cfb5d72bd178
naoto yoshida <yoshida-naoto@social-sharing.org>
2017-08-18 15:17:42 +0900

    test

--- fuel/app/views/confplayer.php
+++ fuel/app/views/confplayer.php
@@ -1,7 +1,7 @@
 <div>
 <?php 
 	echo $mikata_theme[$_SESSION['game']['table']['status']['currentTheme']]['theme'];
-
+	echo '<br>';
 	foreach($_SESSION['game']['hand']['chino'] as $value)
 	{
 		echo $mikata_answer[$value]['answer'];
