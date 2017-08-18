d6ac3d611ec0f17eb33ff2ed99834d1be870169e
naoto yoshida <yoshida-naoto@social-sharing.org>
2017-08-18 16:35:51 +0900

    test

--- fuel/app/views/confplayer.php
+++ fuel/app/views/confplayer.php
@@ -1,11 +1,11 @@
 <div>
 <?php 
+	$i = 1;
 	echo $mikata_theme[$_SESSION['game']['table']['status']['currentTheme']]['theme'];
 	echo '<br>';
 	foreach($_SESSION['game']['hand']['cocoa'] as $value)
 	{
-		$i = 1;
-		echo '<a class="modal_btn cfmanser"'.$i.'>';
+		echo '<a class="modal_btn cfmanser'.$i.'">';
 		echo $mikata_answer[$value]['answer'];
 		echo '</a></br>';
 		echo View::forge('popup/wrap', array('name' => 'cfmanser', 'size' => 'normal', 'popup_data' => $mikata_answer[$value]['answer']));
