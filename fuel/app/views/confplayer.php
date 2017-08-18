68050f9694c3594970ba39e49fd14e93425f4b52
naoto yoshida <yoshida-naoto@social-sharing.org>
2017-08-18 10:40:18 +0900

    test

--- fuel/app/views/confplayer.php
+++ fuel/app/views/confplayer.php
@@ -1,16 +1,7 @@
-<?= print_r($game) ?>
-
 <div>
-	<table>
-		<?php foreach ($mikata_answer as $answer): ?>
-			<tr>
-				<td>
-					<?= $answer['id'] ?>
-				</td>
-				<td>
-					<?= $answer['answer'] ?>
-				</td>
-			</tr>
-		<?php endforeach; ?>
-	</table>
+<?php foreach($game['hand']['chino'] as $value)
+	{
+		echo $mikata_answer[$value]['answer'];
+	}
+?>
 </div>
