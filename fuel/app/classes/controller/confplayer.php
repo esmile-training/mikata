784086bcaaf82f69257c7c2f04e24e4cb542eef0
naoto yoshida <yoshida-naoto@social-sharing.org>
2017-08-18 14:11:08 +0900

    test

--- fuel/app/classes/controller/confplayer.php
+++ fuel/app/classes/controller/confplayer.php
@@ -6,13 +6,10 @@ class Controller_confPlayer extends Controller_Base_Game
 	{	
 		$table = $_SESSION['game']['table'];
 		$themeStock = $_SESSION['game']['deck']['themeStock'];
-		
-		print_r($table);
-		
+				
 		$this->view_data['mikata_theme'] = $this->csv->getAll('/mikata/theme');
-		exit;
 		$this->view_data['mikata_answer'] = $this->csv->getAll('/mikata/answer');
-		
+		exit;
 		//$_SESSION['game']['table']['status']['currentTheme'] = Model_tableinfo::picThemeCard($table[1],$themeStock);
 		
 		return View_Wrap::contents('confplayer',$this->view_data);
