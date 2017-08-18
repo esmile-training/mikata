e1abc3d788852782d9785888773443aa9be7efe1
naoto yoshida <yoshida-naoto@social-sharing.org>
2017-08-18 11:40:24 +0900

    test

--- fuel/app/classes/controller/entername.php
+++ fuel/app/classes/controller/entername.php
@@ -36,7 +36,7 @@ class Controller_entername extends Controller_Base_Game
 		
 		$this->view_data['mikata_answer'] = $this->csv->getAll('/mikata/answer');
         
-		Session::set('game', array($this->game));
+		Session::set('game', array('sattus' => $this->game['status']));
 		
 		//一番目のプレイヤーの待機画面に遷移
 		Response::redirect('confplayer');	//実験的にコメントアウト
