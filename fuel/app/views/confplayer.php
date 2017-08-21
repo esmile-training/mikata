10b32b4347e6198f52676d5a22f82ad8695b07b4
naoto yoshida <yoshida-naoto@social-sharing.org>
2017-08-21 16:21:50 +0900

    test

--- fuel/app/views/confplayer.php
+++ fuel/app/views/confplayer.php
@@ -14,7 +14,7 @@
 		echo '<a class="modal_btn cfmanser '.$count.'">';
 		echo $mikata_answer[$value]['answer'];
 		echo '</a>';
-		echo View::forge('popup/wrap', array('name' => 'cfmanser', 'size' => 'normal', 'popup_data' => $mikata_answer[$value]['answer']));
+		echo View::forge('popup/wrap', array('name' => 'cfmanser '.$count, 'size' => 'normal', 'popup_data' => $mikata_answer[$value]['answer']));
 		echo '</br>';
 		$count++;
 	}
