11b4316efcc424149f494f716a462273cae11087
naoto yoshida <yoshida-naoto@social-sharing.org>
2017-08-21 12:47:25 +0900

    test

--- public/assets/js/modal.js
+++ public/assets/js/modal.js
@@ -8,26 +8,26 @@ var id = '';
 			target = $("#modal-" + target);
 			//3つ目の引数としてidを追加
 			if($(this).get(0).className.split(" ")[2]) id = $(this).get(0).className.split(" ")[2];
-			console.log(id);
+
 			// キーボード操作などにより、オーバーレイが多重起動するのを防止する
 			$(this).blur() ;	//ボタンからフォーカスを外す
 			//新しくモーダルウィンドウを起動しない
-			if($("#modal-overlay"+id)[0]) return false ;
+			if($("#modal-overlay")[0]) return false ;
 
 			// オーバーレイ用のHTMLコードを、[container]内の最後に生成する
-			$("#container").append('<div id="modal-overlay'+id+'></div>') ;
-			$("#modal-overlay"+id).fadeIn("slow") ;
+			$("#container").append('<div id="modal-overlay></div>') ;
+			$("#modal-overlay").fadeIn("slow") ;
 
 			// コンテンツをフェードインする
 			$(target).fadeIn( "slow" ) ;
 		}
 	);
 	//[#modal-close]をクリックしたら…
-	$("#modal-overlay"+id+",#modal-close").unbind().click( function(){
+	$("#modal-overlay,#modal-close").unbind().click( function(){
 		//[#modal-content]と[#modal-overlay]をフェードアウトした後に…
-		$(target,"#modal-overlay"+id).fadeOut("slow" , function(){
+		$(target,"#modal-overlay").fadeOut("slow" , function(){
 			//[#modal-overlay]を削除する
-			$('#modal-overlay'+id).remove();
+			$('#modal-overlay').remove();
 		});
 	});
 });
