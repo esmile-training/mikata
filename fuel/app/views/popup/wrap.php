818be2cfd9240a594aa10f496553210dc170cc6e
naoto yoshida <yoshida-naoto@social-sharing.org>
2017-08-21 14:05:33 +0900

    test

--- fuel/app/views/popup/wrap.php
+++ fuel/app/views/popup/wrap.php
@@ -1,8 +1,6 @@
 <div id="modal-<?= $name ?>" class="modal-content-<?= $size ?>">
 	<?php //引き渡しデータがある場合 ?>
 	<?php if(isset($popup_data)){ ?>
-	<?php echo $popup_data;
-	 ?>
 		<?= View::forge('popup/'.$name, array('popup_data'=>$popup_data)) ?>
 	<?php //引き渡しデータがない場合 ?>
 	<?php }else{ ?>
