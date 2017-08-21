9743b071526ba09512ec228af195273807b85cdd
naoto yoshida <yoshida-naoto@social-sharing.org>
2017-08-21 14:57:36 +0900

    test

--- public/assets/js/modal.js
+++ public/assets/js/modal.js
@@ -7,8 +7,8 @@ var id = '';
 			target = $(this).get(0).className.split(" ")[1];
 			//3つ目の引数としてidを追加
 			if($(this).get(0).className.split(" ")[2]) id = $(this).get(0).className.split(" ")[2];
-			target = $("#modal-" + target/* + " " + id*/);
-			console.log(target);
+			target = $("#modal-" + target + " " + id);
+			console.log(id);
 			// キーボード操作などにより、オーバーレイが多重起動するのを防止する
 			$(this).blur() ;	//ボタンからフォーカスを外す
 			//新しくモーダルウィンドウを起動しない
