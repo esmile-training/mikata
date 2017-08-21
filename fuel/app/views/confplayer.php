0ed2466fada43e4055a33ed4d00c70aea95a31cd
naoto yoshida <yoshida-naoto@social-sharing.org>
2017-08-21 17:52:30 +0900

    test

--- fuel/app/views/confplayer.php
+++ fuel/app/views/confplayer.php
@@ -1,17 +1,11 @@
 <div>
-	<script type="text/javascript">
-		function modalup(){
-			$(this).attr('modal_btn', 'cfmanser');
-		}
-	</script>
-		
 <?php 
 	$count = 1;
 	echo $mikata_theme[$_SESSION['game']['table']['status']['currentTheme']]['theme'];
 	echo '<br>';
 	foreach($_SESSION['game']['hand']['cocoa'] as $value)
 	{
-		echo '<a onclick = "modalup(); return false;">';
+		echo '<a class="modal_btn cfmanser '.$count.'">';
 		echo $mikata_answer[$value]['answer'];
 		echo '</a>';
 		echo View::forge('popup/wrap', array('name' => 'cfmanser', 'size' => 'normal', 'popup_data' => $mikata_answer[$value]['answer']));
--- fuel/app/views/popup/cfmanser.php
+++ fuel/app/views/popup/cfmanser.php
@@ -1,3 +1,7 @@
 <div>
+	<?= $mikata_theme[$_SESSION['game']['table']['status']['currentTheme']]['theme']; ?>
+</div>
+
+<div>
 	<?= $popup_data ?>
 </div>
