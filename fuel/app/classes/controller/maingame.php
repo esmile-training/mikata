dc361eeb046540f9731016115f428bead460af1d
naoto yoshida <yoshida-naoto@social-sharing.org>
2017-08-22 10:08:40 +0900

    test

--- fuel/app/classes/controller/maingame.php
+++ fuel/app/classes/controller/maingame.php
@@ -1,40 +1,17 @@
 <?php
 
-class Controller_entername extends Controller_Base_Game
-{	
+class Controller_maingame extends Controller_Base_Game
+{
 	public function action_index()
-	{
-		View_Wrap::contents('entername',$this->view_data);
-	}
-	
-	public function action_setPlayerName()
-	{
-		$param = input::post();
+	{	
+		$table = $_SESSION['game']['table'];
+		$themeStock = $_SESSION['game']['deck']['themeStock'];
 		
-		//ラウンド数、現在のテーマID、現在の操作プレイヤーの配列
-		$this->game['table']['status'] = array('roundCount' => Model_tableinfo::$roundCount, 'currentTheme' => Model_tableinfo::$currentTheme, 'currentPlayer' => Model_tableinfo::$currentPlayer);
+		$this->view_data['mikata_theme'] = $this->csv->getAll('/mikata/theme');
+		$this->view_data['mikata_answer'] = $this->csv->getAll('/mikata/answer');
 		
-		//プレイヤーが出したそれカードの格納配列
-		$this->game['table']['thatCardArray'] = Model_tableinfo::setThatArray();
+		$_SESSION['game']['table']['status']['currentTheme'] = Model_tableinfo::picThemeCard($table['status']['roundCount'],$themeStock);
 		
-		//投票用の配列
-		$this->game['table']['votesArray'] = Model_tableinfo::setVotesArray();
-		 
-		//プレイヤー名を配列に格納
-		$this->game['table']['player'] = Model_tableinfo::setPlayerArrayValue($param);
-		
-		//プレイヤーのスコア格納配列
-		$this->game['table']['playerScoreArray'] = Model_tableinfo::setScoreArray();
-		
-		//山札をシャッフルする
-		$this->game['deck'] = Model_Deckinfo::shuffleCard();
-		
-		//山札から手札を配る
-		$this->game['hand'] = Model_Handinfo::createPlayerHand();
-		
-		$_SESSION['game'] = $this->game;
-		
-		//一番目のプレイヤーの待機画面に遷移
-		Response::redirect('confplayer');
+		return View_Wrap::contents('confplayer',$this->view_data);
 	}
 }
