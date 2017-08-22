4d7bfa18ff66d35f92a550094e4612939a886fce
naoto yoshida <yoshida-naoto@social-sharing.org>
2017-08-22 15:04:36 +0900

    add maingame

--- fuel/app/views/maingame/selectanswer.php
+++ fuel/app/views/maingame/selectanswer.php
@@ -3,6 +3,8 @@
 	$count = 1;
 	echo $mikata_theme[$game['table']['status']['currentTheme']];
 	echo '<br>';
+	print_r($game['hand']);
+	exit;
 	foreach($game['hand'][$game['table']['playerArray'][$game['table']['status']['currentPlayer']]] as $value)
 	{
 		echo '<a class="modal_btn cfmanser '.$count.'">';
