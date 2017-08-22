d36c033a5d475599e1330e7e75723c73f12998f2
naoto yoshida <yoshida-naoto@social-sharing.org>
2017-08-22 11:41:56 +0900

    test

--- fuel/app/classes/controller/entername.php
+++ fuel/app/classes/controller/entername.php
@@ -11,30 +11,12 @@ class Controller_entername extends Controller_Base_Game
 	{
 		$param = input::post();
 		
-		//ラウンド数、現在のテーマID、現在の操作プレイヤーの配列
-		$this->game['table']['status'] = array('roundCount' => Model_tableinfo::$roundCount, 'currentTheme' => Model_tableinfo::$currentTheme, 'currentPlayer' => Model_tableinfo::$currentPlayer);
-		
-		//プレイヤーが出したそれカードの格納配列
-		$this->game['table']['thatCardArray'] = Model_tableinfo::setThatArray();
-		
-		//投票用の配列
-		$this->game['table']['votesArray'] = Model_tableinfo::setVotesArray();
-		 
 		//プレイヤー名を配列に格納
 		$this->game['table']['player'] = Model_tableinfo::setPlayerArrayValue($param);
-		
-		//プレイヤーのスコア格納配列
-		$this->game['table']['playerScoreArray'] = Model_tableinfo::setScoreArray();
-		
-		//山札をシャッフルする
-		$this->game['deck'] = Model_Deckinfo::shuffleCard();
-		
-		//山札から手札を配る
-		$this->game['hand'] = Model_Handinfo::createPlayerHand();
-		
+			
 		$_SESSION['game'] = $this->game;
 		
 		//一番目のプレイヤーの待機画面に遷移
-		Response::redirect('confplayer');
+		Response::redirect('maingame');
 	}
 }
--- fuel/app/classes/controller/maingame.php
+++ fuel/app/classes/controller/maingame.php
@@ -4,14 +4,39 @@ class Controller_maingame extends Controller_Base_Game
 {
 	public function action_index()
 	{	
-		$table = $_SESSION['game']['table'];
-		$themeStock = $_SESSION['game']['deck']['themeStock'];
+		//ラウンド数、現在のテーマID、現在の操作プレイヤーの配列
+		$this->game['table']['status'] = array('roundCount' => Model_tableinfo::$roundCount, 'currentTheme' => Model_tableinfo::$currentTheme, 'currentPlayer' => Model_tableinfo::$currentPlayer);
 		
+		//プレイヤーが出したそれカードの格納配列
+		$this->game['table']['thatCardArray'] = Model_tableinfo::setThatArray();
+		
+		//投票用の配列
+		$this->game['table']['votesArray'] = Model_tableinfo::setVotesArray();
+		
+		//プレイヤーのスコア格納配列
+		$this->game['table']['playerScoreArray'] = Model_tableinfo::setScoreArray();
+		
+		//山札をシャッフルする
+		$this->game['deck'] = Model_Deckinfo::shuffleCard();
+		
+		//山札から手札を配る
+		$this->game['hand'] = Model_Handinfo::createPlayerHand();
+		
+		//csvファイルの読み込み
 		$this->view_data['mikata_theme'] = $this->csv->getAll('/mikata/theme');
 		$this->view_data['mikata_answer'] = $this->csv->getAll('/mikata/answer');
 		
-		$_SESSION['game']['table']['status']['currentTheme'] = Model_tableinfo::picThemeCard($table['status']['roundCount'],$themeStock);
+		//お題カードを引く
+		$this->game['table']['status']['currentTheme'] = Model_tableinfo::picThemeCard($this->game['table']['roundCount'],$this->game['deck']['themeStock']);
 		
-		return View_Wrap::contents('confplayer',$this->view_data);
+		//山札から一枚回答カードを引く
+		$this->game['table']['thatCardArray']['0'] = Model_tableinfo::picThatCard($this->game['deck']['themeStock']);
+		
+		return View_Wrap::contents('checkplayer',$this->view_data);
+	}
+	
+	public function action_enterAnswer()
+	{
+		$param = input::post();
 	}
 }
