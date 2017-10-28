9947cbe94c8a2277786970a11b111ee2dcbbc5f7
naoto yoshida <yoshida-naoto@social-sharing.org>
2017-10-28 10:21:07 +0900

    selectanswer

--- fuel/app/views/maingame/selectanswer.php
+++ fuel/app/views/maingame/selectanswer.php
@@ -1,16 +1,15 @@
 <div>
-<?php
-	$count = 1;
-	echo $mikata_theme[$game['table']['status']['currentTheme']]['theme'];
-	echo '<br>';
-	foreach($game['hand'][$game['table']['status']['currentPlayer']] as $value)
-	{
-		//echo '<a class="modal_btn cfmanser '.$count.'">';
-		echo '<a class="modal_btn cfmanser">';
-		echo '<a>'.$mikata_answer[$value]['answer'].'</a>';
-		echo View::forge('popup/wrap', array('name' => 'checkanswer', 'size' => 'normal', 'popup_data' => $mikata_answer[$value]['answer']));
-		echo '</br>';
-		$count++;
-	}
-?>
+	<?php $count = 1; ?>
+	<?= $mikata_theme[$game['table']['status']['currentTheme']]['theme']; ?>
+		<br>
+	<?php foreach ($game['hand'][$game['table']['status']['currentPlayer']] as $value): ?>
+		
+		<a class="modal_btn checkanswer"><?= $mikata_answer[$value]['answer'] ?></a>
+		<?= View::forge('popup/wrap', array('name' => 'checkanswer','size' => 'normal', 'popup_data' => $mikata_answer[$value]['answer'])); ?>
+		
+		<?php $count++; ?>
+		
+		<br>
+		
+	<?php endforeach; ?>
 </div>
