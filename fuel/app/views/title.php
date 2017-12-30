9865915650fb5854ef550fd2169ab6bd114b3dab
naoto <naoto@tanu-five>
2017-12-31 00:50:29 +0900

    style title

--- fuel/app/views/title.php
+++ fuel/app/views/title.php
@@ -1,9 +1,30 @@
 <style type="text/css">
-	div{
+	div#container{
 		background-image: url("http://esmile-sys.sakura.ne.jp/Mikata/img/bg_title.jpg");
 		background-size: contain;
 	}
+        div#logo{
+            position: absolute;
+            left: 7%;
+        }
+        img#logo{
+            max-width: 800px;
+            height: auto;
+        }
+        div#start_btn{
+            position: absolute;
+            top: 68.5%;
+            left: 27.5%;
+            background-image: none;
+        }
+        img#start_btn{
+            max-height: 108px;
+            width: auto;
+        }
 </style>
-<H1>私の世界の見方(仮題)</H1>
-<button onclick="location.href='setplayernumber'">スタート</button>
-<a href='setplayernumber'><img <?= Asset::img('start_1.png',array('id' =>'')) ?> alt="スタート"></a>
+<div id="logo">
+    <?= asset::img('logo.png', array('id'=>'logo', 'alt' => 'logo')) ?>
+</div>
+<div id="start_btn">
+    <a href='setplayernumber'><?= Asset::img('start_1.png',array('id' =>'start_btn','alt' => 'スタート')) ?></a>
+</div>
