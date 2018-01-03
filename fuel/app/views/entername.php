bcfd607072a63cba8c59a00b52e35630974394c5
frandle_2 <frandle_2@Lvateinn>
2018-01-04 01:13:05 +0900

    Merge origin/master
    Conflicts:
    	fuel/app/views/maingame/checkplayer.php

--- fuel/app/views/entername.php
+++ fuel/app/views/entername.php
@@ -1,10 +1,39 @@
+<style type="text/css">
+	div#container{
+		background-image: url("http://esmile-sys.sakura.ne.jp/Mikata/img/bg_y.jpg");
+		background-size: contain;
+	}
+        H1{
+            color: white;
+        }
+        div.input{
+            color: white;
+        }
+        div#form{
+            background-image: none;
+            position: absolute;
+            left: 39%;
+        }
+        text#form{
+            color: white;
+        }
+        input#form_submit{
+            width: 210px;
+            height: auto;
+            margin-top: 79%;
+        }
+        select#name{
+            top: auto;
+        }
+</style>
 <h1>プレイヤー名を順番に入力してください</h1>
-<div>
+<div id="form">
 	<?= Form::open(array('action' => 'entername/setPlayerName', 'method' => 'post'));
 		for ($n = 1; $n <= Model_tableinfo::$playerNumber; $n++)
 		{
-			print_r('プレイヤー'.$n.' <input type="text" name='.$n.'><br>');
+			print_r('<text id="form">プレイヤー'.$n.' </text>');
+                        print_r('<input type="text" name='.$n.'><br>');
 		} ?>
-	<?=	Form::button(null, '決定', array('type' => 'submit', 'style' => 'padding: 2px;')); ?>
+    <?= Form::input(array('type' => 'image','name' => 'submit', 'src' => 'http://esmile-sys.sakura.ne.jp/Mikata/yoshida/public/assets/img/ok_y_1.png')); ?>
 	<?=	Form::close(); ?>
 </div>
--- fuel/app/views/setplayernumber.php
+++ fuel/app/views/setplayernumber.php
@@ -1,16 +1,29 @@
 <style type="text/css">
-	div{
+	div#container{
 		background-image: url("http://esmile-sys.sakura.ne.jp/Mikata/img/bg_b_1.jpg");
 		background-size: contain;
 	}
         H1{
             color: white;
         }
+        div#form{
+            background-image: none;
+            position: absolute;
+            left: 39%;
+        }
+        input#form_submit{
+            width: 210px;
+            height: auto;
+            margin-top: 250px;
+        }
+        select#name{
+            top: auto;
+        }
 </style>
 <h1>プレイ人数を決めてください。</h1>
-<div style="background-image: none">
+<div id="form">
     <?= Form::open(array('action' => 'setplayernumber/getPlayerNumber', 'method' => 'post'));?>
-        <select name= player>
+        <select name= player id="form">
         <option value= 3>3人</option>
         <option value= 4>4人</option>
         <option value= 5>5人</option>
@@ -19,6 +32,7 @@
         <option value= 8>8人</option>
         <option value= 9>9人</option>
         </select>
+    <br>
     <?= Form::input(array('type' => 'image','name' => 'submit', 'src' => 'http://esmile-sys.sakura.ne.jp/Mikata/yoshida/public/assets/img/ok_b_1.png')) ?>
     <?= Form::close(); ?>
 </div>
