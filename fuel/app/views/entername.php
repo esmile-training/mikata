129f95341c73d8f1e3fedc0437b1b85c96e1d159
frandle_2 <frandle_2@Lvateinn>
2018-01-06 23:29:10 +0900

    update entername css

--- fuel/app/views/entername.php
+++ fuel/app/views/entername.php
@@ -17,10 +17,14 @@
         text#form{
             color: white;
         }
+        div#form_submit{
+            position: absolute;
+            left: 39%;
+            bottom: 10%;
+        }
         input#form_submit{
-            width: 210px;
+            width: 213px;
             height: auto;
-            margin-top: 79%;
         }
         select#name{
             top: auto;
@@ -28,12 +32,14 @@
 </style>
 <h1>プレイヤー名を順番に入力してください</h1>
 <div id="form">
-	<?= Form::open(array('action' => 'entername/setPlayerName', 'method' => 'post'));
-		for ($n = 1; $n <= Model_tableinfo::$playerNumber; $n++)
-		{
-			print_r('<text id="form">プレイヤー'.$n.' </text>');
-                        print_r('<input type="text" name='.$n.'><br>');
-		} ?>
-    <?= Form::input(array('type' => 'image','name' => 'submit', 'src' => 'http://esmile-sys.sakura.ne.jp/Mikata/yoshida/public/assets/img/ok_y_1.png')); ?>
-	<?=	Form::close(); ?>
+    <?= Form::open(array('action' => 'entername/setPlayerName', 'method' => 'post'));
+    for ($n = 1; $n <= Model_tableinfo::$playerNumber; $n++)
+    {
+        print_r('<text id="form">プレイヤー'.$n.' </text>');
+        print_r('<input type="text" name='.$n.'><br>');
+    } ?>
 </div>
+<div id="form_submit">
+    <?= Form::input(array('type' => 'image','name' => 'submit', 'src' => 'http://esmile-sys.sakura.ne.jp/Mikata/yoshida/public/assets/img/ok_y_1.png')); ?>
+</div>
+<?= Form::close(); ?>
