5cef1a8a4fba961f5098ba81ac15ce18013c092b
frandle_2 <frandle_2@Lvateinn>
2018-03-24 02:38:24 +0900

    change title

--- fuel/app/views/common/header.php
+++ fuel/app/views/common/header.php
@@ -2,7 +2,7 @@
 <html>
 <head>
 	<meta http-equiv="Content-Type" content="text/html;charset=utf-8" />
-	<title>私の世界の見方</title>
+	<title>LAUGH INVADERS</title>
 	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
 	<?php echo Asset::js("ajax.js"); ?>
 </head>
